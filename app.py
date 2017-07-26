01c6a822371a6eba5526da319bcce0fae31da790
Aidan Kinzett <akinzett@gmail.com>
2017-07-26 15:04:30 +1000

    test flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+import sqlite3
+import yaml
+import os
+
+# open the configuration file and save config as constants
+with open("config.yml", 'r') as ymlfile:
+    CONFIG = yaml.load(ymlfile)
+
+DOWNLOAD_DIRECTORY = CONFIG['download directory']
+DB_PATH = os.path.join(DOWNLOAD_DIRECTORY, 'echodownloader.db')
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_video_info():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM urls WHERE `Subject Code` = 'MXB102'")
+    videos = cursor.fetchall()
+
+    return render_template('index.html', videos=videos)
