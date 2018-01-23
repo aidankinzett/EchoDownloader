85c5c58815346f750313879b30b2ed964a3dcf32
Aidan Kinzett <hello@aidankinzett.com>
2018-01-23 20:54:00 +1000

    fixed mark as unwatched button

--- static/js/app.js
+++ static/js/app.js
@@ -55,11 +55,10 @@ function mark_video_as_watched(guid) {
 }
 
 function mark_video_as_unwatched(guid) {
-    socket.emit('mark_watched', guid);
+    socket.emit('mark_unwatched', guid);
     document.getElementById(guid + "-newbadge").style.visibility = "visible";
     document.getElementById(guid + "-watched").style.display = "inline";
     document.getElementById(guid + "-unwatched").style.display = "none";
-
 }
 
 socket.on('downloading', function (data) {
